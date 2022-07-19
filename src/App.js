354e6d611017444e14f597eff132b87f9118e0aa
Abhinav-Modi <abhinavmodi07@gmail.com>
2022-07-19 23:07:23 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import Card from "./components/Card";
 import "./App.css";
+import { GoMarkGithub } from "react-icons/go";
+import { FaTwitter } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
+
 const cardImages = [
 	{ src: "/img/helmet-1.png", matched: false },
 	{ src: "/img/potion-1.png", matched: false },
@@ -17,6 +21,8 @@ const App = () => {
 	const [turns, setTurns] = useState(0);
 	const [choiceOne, setChoiceOne] = useState(null);
 	const [choiceTwo, setChoiceTwo] = useState(null);
+	//disable other cards, will only allow two cards to be flipped at a time
+	const [disabled, setDisabled] = useState(false);
 	//creating a function to shuffle the cards
 	const shuffleCards = () => {
 		//duplicating every card
@@ -24,6 +30,8 @@ const App = () => {
 			.sort(() => Math.random() - 0.5)
 			.map((card) => ({ ...card, id: Math.random() }));
 		//updating the state of the cards after shuffling
+		setChoiceOne(null);
+		setChoiceTwo(null);
 		setCards(shuffledCards);
 		setTurns(0);
 	};
@@ -34,6 +42,7 @@ const App = () => {
 	//compare the cards
 	useEffect(() => {
 		if (choiceOne && choiceTwo) {
+			setDisabled(true);
 			if (choiceOne.src === choiceTwo.src) {
 				setCards((prevCards) => {
 					return prevCards.map((card) => {
@@ -47,16 +56,22 @@ const App = () => {
 				});
 				resetTurns();
 			} else {
-				console.log("not match");
-				resetTurns();
+				//delay after choosing the second card
+				setTimeout(() => resetTurns(), 800);
 			}
 		}
 	}, [choiceOne, choiceTwo]);
+	// automatic start game
 	const resetTurns = () => {
 		setChoiceOne(null);
 		setChoiceTwo(null);
 		setTurns(turns + 1);
+		setDisabled(false);
 	};
+	useEffect(() => {
+		shuffleCards();
+	}, []);
+
 	console.log(cards);
 	return (
 		<div className="App">
@@ -71,8 +86,38 @@ const App = () => {
 						card={card}
 						handleChoice={handleChoice}
 						flipped={card === choiceOne || card === choiceTwo || card.matched}
+						disabled={disabled}
 					/>
 				))}
+			</div>
+			<p>Turns:{turns}</p>
+			<div className="footer">
+				<div className="name">
+					<p>Abhinav Modi</p>
+				</div>
+				<div className="links">
+					<a
+						className="footer-link"
+						href="https://github.com/Abhinav-Modi"
+						target="blank"
+					>
+						<GoMarkGithub />
+					</a>
+					<a
+						className="footer-link"
+						href="https://www.linkedin.com/in/abhinav-modi-5312221b5/"
+						target="blank"
+					>
+						<BsLinkedin />
+					</a>
+					<a
+						className="footer-link"
+						target="blank"
+						href="https://twitter.com/Abhinav45529123"
+					>
+						<FaTwitter />
+					</a>
+				</div>
 			</div>
 		</div>
 	);
